0a8b8f01c3839482645d86f9ca9a8478d39c3204
HanDaWoon <bookpia.han@gmail.com>
2018-10-31 15:21:38 +0900

    c_basic ch.5

--- ex_5_09.c
+++ ex_5_09.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
+int Temperatures(double f)
+{
+    double c, k;
+
+    c = (5.0 / 9.0) * (f - 32.0);
+    k = c + 273.16;
+
+    printf("Fahrenheit: %.2f = Celsius: %.2f = Kelvin: %.2f", f, c, k);
+}
+
 int main()
 {
-    float f, c, K;
+    double f;
 
-    scanf("%f", &f);
+    scanf("%lf", &f);
 
-    c = (float) (5.0 / 9.0 * (f - 32.0));
-    K = (float) (c + 273.16);
-
-    printf("Fahrenheit: %.2f = Celsius: %.2f = Kelvin: %.2f", f, c, K);
+    Temperatures(f);
 
     return 0;
 }
