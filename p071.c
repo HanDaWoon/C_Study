42a641f4f5cc93f7a2175684c99abe988ead713c
HanDaWoon <bookpia.han@gmail.com>
2018-11-21 00:18:18 +0900

    c_basic ch.9

---
+++ p071.c
@@ -0,0 +1,27 @@
+//
+// Created by HDW on 2018-11-20.
+//
+
+#include <stdio.h>
+
+void to_base_n(long long, int);
+int main()
+{
+    long long n;
+    int b;
+
+    scanf("%lld %d", &n, &b);
+    to_base_n(n, b);
+
+    return 0;
+}
+
+void to_base_n(long long n, int b)
+{
+    if (n == 0)
+        return;
+
+    to_base_n(n / b, b);
+
+    printf("%lld", n % b);
+}
