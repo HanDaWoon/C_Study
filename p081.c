7dd1f74251561eb6a69d2014171d515256d9c48d
HanDaWoon <bookpia.han@gmail.com>
2018-12-06 18:29:50 +0900

    c_basic ch.10

--- p081.c
+++ p081.c
@@ -3,6 +3,32 @@
 //
 
 #include <stdio.h>
+#include <math.h>
 
 int main()
-{}
+{
+    int N, dis = 0, tmp = 0;
+
+    scanf("%d", &N);
+
+    int X[N], Y[N];
+
+    for (int i = 0; i < N; ++i)
+    {
+        scanf("%d %d", &X[i], &Y[i]);
+    }
+
+    for (int j = 0; j < N; ++j)
+    {
+        for (int k = 0; k < N; ++k)
+        {
+            dis = abs(X[j] - X[k]) + abs(Y[j] - Y[k]);
+            if (dis > tmp)
+                tmp = dis;
+        }
+    }
+
+    printf("%d", tmp);
+
+    return 0;
+}
